11f97ab324dfe9d3d89d45d2ad157f3136f272cf
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-11-10 00:13:08 +0900

    feat:testコードの追記

--- ssh/tmp/test.py
+++ ssh/tmp/test.py
@@ -1,1 +1,18 @@
-print("test")
+import os
+
+path1 = "~/.ssh/authorized_keys"
+path2 = "/etc/ssh/sshd_config"
+if os.path.isfile(path1):
+    print("OK")
+else:
+    print("NO")
+
+if os.path.isfile(path2):
+    print("OK")
+    f = open(path2, "r")
+    contexts = f.read()
+    if "PasswordAuthentication no" in contexts:
+        print("OK")
+    f.close()
+else:
+    print("NO")
